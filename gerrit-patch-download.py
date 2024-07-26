eb034e29770be2845d918670372cf0a9ac08c813
hidenorly <twitte.harold@gmail.com>
2024-07-27 02:42:31 +0900

    Add download functionality

--- gerrit-patch-download.py
+++ gerrit-patch-download.py
@@ -4,14 +4,32 @@ import argparse
 from datetime import datetime, timedelta
 from gerrit_query import GerritUtil
 
+def download_git(base_dir, id, download_cmd):
+    # Create the target folder
+    target_folder = os.path.join(base_dir, str(id))
+    os.makedirs(target_folder, exist_ok=True)
+    current_dir = target_folder
+
+    commands = download_cmd.split(';')
+    for command in commands:
+        command = command.strip()
+        if command.startswith('cd'):
+            # Change directory command
+            new_dir = command[3:].strip()
+            current_dir = os.path.join(current_dir, new_dir)
+        else:
+            # Execute other commands
+            subprocess.run(command, shell=True, check=True, cwd=current_dir)
+
+GerritUtil.download_git = download_git
 
 def main():
-    parser = argparse.ArgumentParser(description='Query Gerrit and parse results')
+    parser = argparse.ArgumentParser(description='Download gerrit patch')
     parser.add_argument('-t', '--target', default='gerrit-ssh', help='Specify ssh target host')
     parser.add_argument('-b', '--branch', default='main', help='Branch to query')
     parser.add_argument('-s', '--status', default='merged|open', help='Status to query (merged|open)')
     parser.add_argument('--since', default='1 week ago', help='Since when to query')
-    parser.add_argument('-d', '--download', default='.', help='Specify')
+    parser.add_argument('-d', '--download', default='.', help='Specify download path')
     args = parser.parse_args()
 
     result = GerritUtil.query(args.target, args.branch, args.status, args.since)
@@ -23,6 +41,7 @@ def main():
                 for key, value in _data.items():
                     print(f'{key}:{value}')
                 print("")
+                GerritUtil.download(args.download, _data["id]"], _data["patchset1_ssh"])
 
 if __name__ == "__main__":
     main()
