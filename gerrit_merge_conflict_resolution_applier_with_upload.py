7a3d1f0b5362b9734f4507514c77fc68fb66c60d
hidenorly <twitte.harold@gmail.com>
2024-08-25 08:34:57 +0900

    Add -n support

--- gerrit_merge_conflict_resolution_applier_with_upload.py
+++ gerrit_merge_conflict_resolution_applier_with_upload.py
@@ -63,6 +63,8 @@ def main():
     parser.add_argument('-b', '--branch', default=os.getenv("GERRIT_BRANCH", 'main'), help='Branch to query')
     parser.add_argument('-s', '--status', default='merged|open', help='Status to query (merged|open)')
     parser.add_argument('--since', default='1 week ago', help='Since when to query')
+    parser.add_argument('-n', '--numbers', default="", action='store', help='Specify gerrit numbers with ,')
+
     parser.add_argument('-w', '--download', default='.', help='Specify download path')
     parser.add_argument('-r', '--renew', default=False, action='store_true', help='Specify if re-download anyway')
     parser.add_argument('-m', '--marginline', default=10, type=int, action='store', help='Specify margin lines')
@@ -100,7 +102,7 @@ def main():
     solver = MergeConflictSolver(gpt_client, args.promptfile)
     applier = MergeConflictResolutionApplier(args.marginline)
 
-    result = GerritUtil.query(args.target, args.branch, args.status, args.since)
+    result = GerritUtil.query(args.target, args.branch, args.status, args.since, args.numbers.split(","))
     for project, data in result.items():
         for branch, theData in data.items():
             for _data in theData:
@@ -112,17 +114,15 @@ def main():
                 print("")
                 download_path = GitUtil.download(args.download, _data["number"], _data["patchset1_ssh"], args.renew)
                 conflict_detector = ConflictExtractor(download_path, args.marginline)
-                _conflict_detector = ConflictExtractor(download_path, 1)
                 conflict_sections = conflict_detector.get_conflicts()
-                _conflict_sections = _conflict_detector.get_conflicts()
                 for file_name, sections in conflict_sections.items():
                     print(file_name)
                     # get resolutions for each conflicted area
                     resolutions = []
                     for i,section in enumerate(sections):
                         print(f'---conflict_section---{i}')
-                        print(_conflict_sections[file_name][i])
-                        resolution, _full_response = solver.query(section)
+                        print(section["section"])
+                        resolution, _full_response = solver.query(section["section"])
                         print(f'---resolution---{i}')
                         print(resolution)
                         codes = applier.get_code_section(resolution)
@@ -131,7 +131,7 @@ def main():
                     # apply resolutions for the file
                     target_file_lines = applier.read_file(file_name)
                     resolutions_lines = list(itertools.chain(*resolutions))
-                    target_file_lines = applier.solve_merge_conflict(target_file_lines, sections, resolutions_lines)
+                    target_file_lines = applier.solve_merge_conflict(target_file_lines, sections, resolutions_lines, resolutions)
                     _target_file_lines = applier.just_in_case_cleanup(target_file_lines)
                     #print(f'---resolved_full_file---{file_name}')
                     #print('\n'.join(target_file_lines))
--- gerrit_patch_downloader.py
+++ gerrit_patch_downloader.py
@@ -53,11 +53,12 @@ def main():
     parser.add_argument('-b', '--branch', default=os.getenv("GERRIT_BRANCH", 'main'), help='Branch to query')
     parser.add_argument('-s', '--status', default='merged|open', help='Status to query (merged|open)')
     parser.add_argument('--since', default='1 week ago', help='Since when to query')
+    parser.add_argument('-n', '--numbers', default="", action='store', help='Specify gerrit numbers with ,')
     parser.add_argument('-d', '--download', default='.', help='Specify download path')
     parser.add_argument('-r', '--renew', default=False, action='store_true', help='Specify if re-download anyway')
     args = parser.parse_args()
 
-    result = GerritUtil.query(args.target, args.branch, args.status, args.since)
+    result = GerritUtil.query(args.target, args.branch, args.status, args.since, args.numbers.split(","))
     for project, data in result.items():
         for branch, theData in data.items():
             for _data in theData:
