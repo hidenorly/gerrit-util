f0b853ce37b9d46e15133b97cdb005a7e91d1f04
hidenorly <twitte.harold@gmail.com>
2024-10-15 02:17:30 +0900

    Add --gitpath, --connection on gerrit_comment_extractor.py

--- gerrit_comment_extractor.py
+++ gerrit_comment_extractor.py
@@ -82,13 +82,17 @@ def main():
     parser.add_argument('-s', '--status', default='merged|open', help='Status to query (merged|open)')
     parser.add_argument('--since', default='1 week ago', help='Since when to query')
     parser.add_argument('-n', '--numbers', default="", action='store', help='Specify gerrit numbers with ,')
+    parser.add_argument('--gitpath', default=None, action='store', help='Specify regexp for project(gitpath) if necessary')
+
+    parser.add_argument('--connection', default="http", action='store', help='Specify ssh or http')
 
     parser.add_argument('-d', '--download', default='.', help='Specify download path')
     parser.add_argument('-r', '--renew', default=False, action='store_true', help='Specify if re-download anyway')
     parser.add_argument('-m', '--marginline', default=10, type=int, action='store', help='Specify margin lines')
     args = parser.parse_args()
 
-    result = GerritUtil.query(args.target, args.branch, args.status, args.since, args.numbers.split(","), ["--comments", "--current-patch-set"])
+    result = GerritUtil.query(args.target, args.branch, args.status, args.since, args.numbers.split(","), ["--comments", "--current-patch-set"], args.connection, args.gitpath)
+
     for project, data in result.items():
         for branch, theData in data.items():
             for _data in theData:
