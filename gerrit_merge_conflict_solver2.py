4157c4ec2af23b6c0bee1e52710303b6c371637e
hidenorly <twitte.harold@gmail.com>
2024-11-07 02:05:21 +0900

    Add --gitpath, connection, gpt on gerrit_merge_conflict_solver2.py

--- gerrit_merge_conflict_solver2.py
+++ gerrit_merge_conflict_solver2.py
@@ -161,6 +161,9 @@ def main():
     parser.add_argument('-s', '--status', default='merged|open', help='Status to query (merged|open)')
     parser.add_argument('--since', default='1 week ago', help='Since when to query')
     parser.add_argument('-n', '--numbers', default="", action='store', help='Specify gerrit numbers with ,')
+    parser.add_argument('--gitpath', default=None, action='store', help='Specify regexp for project(gitpath) if necessary')
+
+    parser.add_argument('--connection', default="http", action='store', help='Specify ssh or http')
 
     parser.add_argument('-w', '--download', default='.', help='Specify download path')
     parser.add_argument('-r', '--renew', default=False, action='store_true', help='Specify if re-download anyway')
@@ -168,6 +171,8 @@ def main():
     parser.add_argument('-l', '--largerconflictsection', default=False, action='store_true', help='Specify if unify overwrapped sections')
 
     parser.add_argument('-c', '--useclaude', action='store_true', default=False, help='specify if you want to use calude3')
+    parser.add_argument('-g', '--gpt', action='store', default="openai", help='specify openai or calude3 or openaicompatible')
+
     parser.add_argument('-k', '--apikey', action='store', default=None, help='specify your API key or set it in AZURE_OPENAI_API_KEY env')
     parser.add_argument('-y', '--secretkey', action='store', default=os.getenv("AWS_SECRET_ACCESS_KEY"), help='specify your secret key or set it in AWS_SECRET_ACCESS_KEY env (for claude3)')
     parser.add_argument('-e', '--endpoint', action='store', default=None, help='specify your end point or set it in AZURE_OPENAI_ENDPOINT env')
@@ -179,7 +184,8 @@ def main():
     gpt_client = GptClientFactory.new_client(args)
     solver = MergeConflictSolver(gpt_client, args.promptfile)
 
-    result = GerritUtil.query(args.target, args.branch, args.status, args.since, args.numbers.split(","))
+    result = GerritUtil.query(args.target, args.branch, args.status, args.since, args.numbers.split(","), [], args.connection, args.gitpath)
+
     for project, data in result.items():
         for branch, theData in data.items():
             for _data in theData:
